4263ced5f59ce53b3e32b4b3357499e7e6dfc54f
sobertrauter <robert_sauter@gmx.de>
2024-02-09 22:32:13 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Button, CircularProgress } from '@mui/material';
+import { AppBar, Button, CircularProgress, Toolbar, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { userService } from './services/UserService';
 import { googleFormsService } from './services/GoogleFormsService';
 import { State } from './lib/AsyncState';
+import { Link as RouterLink } from 'react-router-dom';
+import { Code } from '@mui/icons-material';
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -33,11 +35,25 @@ export default function App() {
 
   return appState === 'success'
     ? <>
-      {loggedIn
-        ? <Button className="absolute top-5 left-5" variant="outlined" onClick={userService.logout}>Logout</Button>
-        : <></>
-      }
-      <Outlet />
+      <AppBar>
+        <Toolbar className="flex items-center justify-between" variant="dense">
+          {loggedIn
+            ? <><div className="flex items-center">
+                <Code />
+                <Typography className="mr-8">HackEval</Typography>
+                <RouterLink to="/">
+                  <Button className="text-white">Overview</Button>
+                </RouterLink>
+              </div>
+              <Button className="text-white border-white" onClick={userService.logout}>Logout</Button>
+            </>
+            : <Typography>HackEval</Typography>
+          }
+        </Toolbar>
+      </AppBar>
+      <div className="mt-16">
+        <Outlet />
+      </div>
     </>
   : appState === 'loading'
     ? <div className="h-screen flex justify-center items-center">
